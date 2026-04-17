db0b8d2517fd3ae6c1707cce76c1be14b5c1a7d9
mareerray <mareerray@gmail.com>
2026-04-17 19:27:04 +0300

    fix: update app name in manifest.json for consistency

--- frontend/build/web/flutter_bootstrap.js
+++ frontend/build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"052f31d115eceda8cbff1b3481fcde4330c4ae
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4227031593" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "87821408" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
